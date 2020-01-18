477434d6cebfaa6aacdf3914c04ce4dab5634aac
maxim.lavrinenko <Maxim.Lavrinenko@norbit.ru>
2020-01-18 22:33:56 +0300

    Add spinner

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,61 +1,80 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service.js'
+import Spinner from '../spinner/spinner'
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
 
-  state = {
-    name: null,
-    population: null,
-    rotationPeriod: null,
-    diameter: null
-  }
+  swapiService = new SwapiService();
 
+  state = {
+    planet: {},
+    loading: true
+  };
 
   constructor() {
     super();
-    setInterval(()=>{this.updateRandomPlanet}, 3000);
+    this.updateRandomPlanet();
+  };
 
-  }
+  onPlanetLoaded = (planet) => {
+    this.setState({
+      planet,
+      loading:false
+    })
+  };
 
   updateRandomPlanet() {
     const id = Math.floor(Math.random()*25+2);
-      const swapi = new SwapiService();
-      swapi.getPlanet(id).then((planet) => {
-        this.setState({
-          name: planet.name,
-          population: planet.population,
-          rotationPerion: planet.rotationPeriod,
-          diameter: planet.diameter
-        });
-      });
+      this.swapiService
+        .getPlanet(id)
+        .then(this.onPlanetLoaded);
   }
-  render() {
-    const {name, population, rotationPeriod, diameter} = this.state;
 
+  render() {
+
+    const { loading, planet } = this.state;
+
+    const spinner = loading ? <Spinner /> : null;
+    const content = !loading ? <PlanetView planet={planet}/> : null;
+      
     return (
       <div className="random-planet jumbotron rounded">
-        <img className="planet-image"
-             src="https://starwars-visualguide.com/assets/img/planets/5.jpg" />
-        <div>
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
+        {spinner}
+        {content}
       </div>
-
     );
   }
 }
+
+const PlanetView = ( { planet } ) => {
+
+  const { name, population, rotationPeriod, 
+    diameter } = planet;
+
+  return (
+    <React.Fragment>
+      <img className="planet-image"
+        src="https://starwars-visualguide.com/assets/img/planets/5.jpg" />
+      <div>
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population</span>
+            <span>{population}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  )
+}
+
+
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './spinner.css'
+
+export default class Spinner extends Component {
+
+render() {
+  return (
+    <div className="spinner-box">
+      <div class="loadingio-spinner-double-ring-bwu2pd1yqzv"><div class="ldio-zxe7nc59hti">
+        <div></div>
+        <div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+      </div></div>
+    </div>
+  )
+}
+}
